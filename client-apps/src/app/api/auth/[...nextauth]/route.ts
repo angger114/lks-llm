2a4df3a65ab878d626fd03665af73f8e840d59c9
Angger114 <anggerbektian26@gmail.com>
2026-05-17 11:41:50 +0800

    huhuuhu

--- client-apps/src/app/api/auth/[...nextauth]/route.ts
+++ client-apps/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,26 @@
-export const runtime = "nodejs";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-import auth from "@/auth";
+const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
 
-export { auth as GET, auth as POST };
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize() {
+        return {
+          id: "1",
+          name: "test",
+          email: "test@test.com",
+        };
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
